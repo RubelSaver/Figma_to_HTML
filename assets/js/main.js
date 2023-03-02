ed7c22d71318561f38457342e3e0b91db5154d2c
Rubel <mdrubelsaver5@gmail.com>
2023-03-03 02:43:49 +0600

    Home Page

---
+++ assets/js/main.js
@@ -0,0 +1,59 @@
+/*
+==============================
+For testimonial_slider
+==============================
+*/
+$('#testimonial_slider').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+    dots:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
+
+/*
+==============================
+End testimonial_slider
+==============================
+*/
+
+
+
+/*
+==============================
+For testimonial_slider
+==============================
+*/
+$('#brand_slider').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+    smartSpeed:650,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
+
+/*
+==============================
+End testimonial_slider
+==============================
+*/
